be3b732b8c5f1802dafe8accc843828f6342b15c
I Wayan Rupadana <rupadanawayan@gmail.com>
2023-08-26 20:37:11 +0800

    add filament-shield

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -68,6 +69,7 @@ class AdminPanelProvider extends PanelProvider
                     ->editor(TinyEditor::class),
                 SpatieLaravelTranslatablePlugin::make()
                     ->defaultLocales(['id']),
+                FilamentShieldPlugin::make()
             ]);
     }
 }
